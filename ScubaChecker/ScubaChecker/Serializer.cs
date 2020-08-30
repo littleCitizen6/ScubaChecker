c984c0815a70c158fdbb503dda28b758a8fc0f3a
citizen6 <theLittleCitizen6@gmail.com>
2020-08-30 20:36:47 +0300

    added implements

---
+++ ScubaChecker/ScubaChecker/Serializer.cs
@@ -0,0 +1,21 @@
+﻿using MamasChecker.Abstract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ScubaChecker
+{
+    public class Serializer<T> : ISerializer<T>
+    {
+        public Stream Serialize(T[] testsToSerialize)
+        {
+            MemoryStream stream = new MemoryStream();
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(stream, testsToSerialize);
+            return stream;
+        }
+    }
+}
---
+++ ScubaChecker/ScubaChecker/TestCase.cs
@@ -0,0 +1,19 @@
+﻿using MamasChecker.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScubaChecker
+{
+    [Serializable]
+    public class TestCase : ITestCase
+    {
+        public string TestContent { get ; set ; }
+        public int Id { get; set; }
+
+        public bool Run()
+        {
+            return true;
+        }
+    }
+}
